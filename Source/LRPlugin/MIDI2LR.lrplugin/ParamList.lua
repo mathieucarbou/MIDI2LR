f579ef08b4d93d61ff6ff72da70bc27b6b548408
mathieucarbou <mathieu.carbou@gmail.com>
2016-10-18 22:40:09 -0400

    :heavy_plus_sign: HSL B&W and ability to move multiple sliders with only one slider

--- Source/LRPlugin/MIDI2LR.lrplugin/ParamList.lua
+++ Source/LRPlugin/MIDI2LR.lrplugin/ParamList.lua
@@ -808,6 +808,7 @@
     "SaturationAdjustmentBlue",
     "SaturationAdjustmentPurple",
     "SaturationAdjustmentMagenta",
+    "SaturationAdjustment",
     "HueAdjustmentRed",
     "HueAdjustmentOrange",
     "HueAdjustmentYellow",
@@ -1273,6 +1274,10 @@
     "Saturation Adjustment Magenta",
     166
   },
+  --[[SaturationAdjustment = {
+    "Saturation Adjustment Magenta",
+    193
+  },]]
   SaturationAdjustmentOrange = {
     "Saturation Adjustment Orange",
     160
@@ -1752,6 +1757,7 @@
   ResetSaturationAdjustmentPurple = "mixerPanel",
   ResetSaturationAdjustmentRed = "mixerPanel",
   ResetSaturationAdjustmentYellow = "mixerPanel",
+  ResetSaturationAdjustment = "mixerPanel",
   ResetShadowTint = "calibratePanel",
   ResetShadows = "adjustPanel",
   ResetSharpenDetail = "detailPanel",
@@ -1778,6 +1784,7 @@
   SaturationAdjustmentPurple = "mixerPanel",
   SaturationAdjustmentRed = "mixerPanel",
   SaturationAdjustmentYellow = "mixerPanel",
+  SaturationAdjustment = "mixerPanel",
   ShadowTint = "calibratePanel",
   Shadows = "adjustPanel",
   SharpenDetail = "detailPanel",
